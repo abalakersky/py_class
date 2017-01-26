dd982741d0d802c7bacddd697f1a72c6261bf903
Alex Balakersky <alex@balakersky.com>
2017-01-26 10:40:51 -0500

    Updates

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,14 +1,15 @@
-# ctrl + Shift + B will run the file.
-print("hello world")
- 
+
+""" ctrl + Shift + B will run the file. """
+
 #Test Importing Numpy
 import numpy as np
-  
+
+print("hello world")
 #define a vector size 4
-a = np.array([1,2,3,4])
+a = np.array([1, 2, 3, 4])
   
 #define a 2x4 matrix
 b = np.array([[ 1, 2, 3, 9], 
-              [ 4, 5, 6, 9]])
+              [ 4, 5, 6, 9]]) 
  
 print( a.dot(b.transpose()) ) 
